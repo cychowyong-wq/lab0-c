8dd540dde26ef6c454451bc68b2de12ba0685e39
ChowYong <cy.chowyong@gmail.com>
2025-12-20 21:26:05 +0800

    Add reverse function
    Reverse by swapping all element's previous and next pointer
    
    Change-Id: I4d989d40681ee7f7c099832fb10e260dd632c867

--- queue.c
+++ queue.c
@@ -215,7 +215,18 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;
+    struct list_head *curr = head;
+    do {
+        struct list_head *tmp = curr->next;
+        curr->next = curr->prev;
+        curr->prev = tmp;
+        curr = tmp;
+    } while (curr != head);
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
